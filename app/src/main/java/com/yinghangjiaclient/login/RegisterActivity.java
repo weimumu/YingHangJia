6814c920ed46b334c350c61e0c17c2f93a08f013
linzibo <673418747@qq.com>
2016-08-20 21:34:07 +0800

    添加底部导航

---
+++ app/src/main/java/com/yinghangjiaclient/login/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.yinghangjiaclient.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.yinghangjiaclient.R;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_first);
+    }
+}
