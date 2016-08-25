ec52b426b212a15c03b9f810a75ef4d1c5a2a54c
庄清惠 <zhuangqhc@163.com>
2016-08-25 21:49:21 +0800

    添加产品的获取和评论功能

---
+++ server/src/service/prod.js
@@ -0,0 +1,30 @@
+/**
+ * Created by zhuangqh on 16/8/25.
+ */
+
+import _ from 'lodash';
+import mongoose from 'mongoose';
+import db from '../models';
+
+const ObjectId = mongoose.Types.ObjectId;
+
+async function addComment(id, comment) {
+  const comment_ = _.pick(comment, ['userId', 'text']);
+
+  await db.prod.update({
+    _id: ObjectId(id),
+  }, {
+    $push: {
+      comments: comment_,
+    }
+  });
+}
+
+async function findAllProd() {
+  return await db.prod.find();
+}
+
+export default {
+  addComment,
+  findAllProd,
+};
