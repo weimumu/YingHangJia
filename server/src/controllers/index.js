2e43dce04d4dfe86e6965cf34c91c379c0ea9438
庄清惠 <zhuangqhc@163.com>
2016-08-29 21:23:05 +0800

    添加后台页面的服务端支持

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -4,6 +4,8 @@
 
 import Router from 'koa-router';
 import compose from 'koa-compose';
+import serve from 'koa-static';
+import path from 'path';
 import userCtrl from './user';
 import prodCtrl from './prod';
 import newsCtrl from './news';
@@ -17,6 +19,7 @@ newsCtrl(router);
 feedBackCtrl(router);
 
 export default () => compose([
+  serve(path.join(__dirname, '..', '..', 'public')),
   router.routes(),
   router.allowedMethods(),
 ]);
